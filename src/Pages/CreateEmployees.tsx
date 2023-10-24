34b19ce72cbd3bb04177d1ba923ecb8ba0883e41
Yoann L'hostis <yoannlhostis@MacBook-Air-de-Yoann.local>
2023-10-24 21:50:53 +0200

    add styled-components and Header

--- src/Pages/CreateEmployees.tsx
+++ src/Pages/CreateEmployees.tsx
@@ -1,3 +1,26 @@
+import { styled } from 'styled-components'
+
+import { Header } from '../Components/Header'
+
+const Container = styled.main`
+  display: flex;
+  flex-direction: column;
+`
+
+const SecondTitle = styled.h2`
+  margin: auto;
+`
+
+const Form = styled.form`
+  margin: auto;
+`
+
 export function CreateEmployees() {
-  return <div>CreateEmployees</div>
+  return (
+    <Container>
+      <Header />
+      <SecondTitle>Create Employee</SecondTitle>
+      <Form action="">form</Form>
+    </Container>
+  )
 }
